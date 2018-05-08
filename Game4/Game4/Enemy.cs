04d7f469c8b752dbab7746ab055b26702c27d364
ErikGamet <38037851+ErikGamet@users.noreply.github.com>
2018-05-08 13:28:06 +0200

    u

--- Game4/Game4/Enemy.cs
+++ Game4/Game4/Enemy.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
-namespace Game4
+namespace ShootEmUp
 {
     public class Enemy
     {
+        Rectangle enemyRectangle;
+        Texture2D enemyRexture;
+        Vector2 scale;
+        Vector2 offset;
+        Color enemyColor;
+
+        float speed;
+
         private int enemyHealth;
 
         public void AI()
